683e5954a7b9c92591d011384b4d8af5c5b2a49b
Noah Gullo <noahgulloingram@gmail.com>
2026-08-13 08:46:03 -0700

    feat:  element appears on click at mouse position

---
+++ frontend/src/components/Dropdown.jsx
@@ -0,0 +1,15 @@
+function Dropdown({x, y}){
+    return (
+        <>
+            <div id="dropdown" style={{
+                position: 'absolute',
+                left: `${x}px`, 
+                top: `${y}px`,
+            }}>
+                <h1>a</h1>
+            </div>
+        </>
+    )
+}
+
+export default Dropdown
--- frontend/src/pages/WaldoPage.jsx
+++ frontend/src/pages/WaldoPage.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import Dropdown from "../components/Dropdown"
+
 function WaldoPage({file,}) {
+  const [popup, setPopup] = useState({visible: false, x: 0, y: 0})
+  const handleClick = (event) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    setPopup({visible: !popup.visible, x: x, y: y});
+  }
+
   return (
     <>
-        <img className="imageGame" src={file} width="1475px" height="1000px"/>
+        <div style={{ position: 'relative', display: 'inline-block' }}> 
+          <img className="imageGame" src={file} width="1475px" height="1000px" onClick={handleClick}/>
+          {popup.visible &&
+            <Dropdown x={popup.x} y={[popup.y]}></Dropdown>
+          }
+        </div>
         <Link to="/">Back to Home</Link>
     </>
   )
